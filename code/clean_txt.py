da986abe5a2f7498ef41b4129cd022c3341f62a4
Barry0121 <Barry0121@users.noreply.github.com>
2021-05-05 17:58:44 -0700

    added comment and docstring for clean_txt.py

--- code/clean_txt.py
+++ code/clean_txt.py
@@ -1,6 +1,6 @@
-import numpy as np 
-import matplotlib.pyplot as plt 
-import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 import os
 import re
 import nltk
@@ -11,10 +11,13 @@ from nltk.stem.porter import PorterStemmer
 def clean_lines(astring):
     '''
     Stem words and removing stop words
+
+    :param astring: raw string with stop words and un-stemmed words
+    :return: lowered, without stop word, stemmed version of astring
     '''
     # https://stackoverflow.com/questions/24517722/how-to-stop-nltk-stemmer-from-removing-the-trailing-e
     # https://www.nltk.org/howto/stem.html
-    line = re.sub('[^a-zA-Z]', ' ',astring)
+    line = re.sub('[^a-zA-Z]', ' ',astring) # check letter are valid
     line = line.lower()
     line = line.split()
     # stem words or turn all tenses to present tense
@@ -26,7 +29,16 @@ def clean_lines(astring):
     line = ' '.join(line)
     return line
 
-def clean_txt(fp, scene_delimiter):
+def clean_txt(fp, scene_delimiter, start=0, end=None):
+    """
+    Read in txt file from path and extract characters, dialogue, and stage setup
+
+    :param fp: string location of the txt file
+    :param scene_delimiter: a 2 elements tuple of character symbol containing the stage setup instruction
+    :param start: default={0}, start of the character dialogue
+    :param end: default={None}, end of the character dialogue
+    :return: a dataframe containing the character, dialogue, and stage setup information
+    """
     chars = []
     words = []
     # scene_setup = []
@@ -37,6 +49,8 @@ def clean_txt(fp, scene_delimiter):
     line_nums = []
     with open(fp, 'r', encoding='utf-8') as infile:
         for num_l, line in enumerate(infile):
+            if (num_l < start) or ((num_l > end) and (end != None)):
+                continue
             num_l += 1
             s = line
             if not new_char:
@@ -54,7 +68,7 @@ def clean_txt(fp, scene_delimiter):
 
                 new_char = False
 
-    chars_words = np.array(list(zip(chars, words,line_nums)))    
+    chars_words = np.array(list(zip(chars, words,line_nums)))
     # 2) put lines into a df & store it
     draft = pd.DataFrame(chars_words,columns = ['chars','lines','line_num'])
     # 3) add scene setup column & remove it from current line
@@ -64,6 +78,7 @@ def clean_txt(fp, scene_delimiter):
 
 def get_scene_setup(string,left,right):
     '''
+    helper for clean_txt()
     get scence setup
     '''
     s = string
@@ -74,6 +89,7 @@ def get_scene_setup(string,left,right):
 
 def remove_scene_setup(string,left,right):
     '''
+    helper for clean_txt()
     remove scence setup
     '''
     s = string
